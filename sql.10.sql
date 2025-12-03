3893d221481a38a0fbf4709cdf349ca16d5b50d0
Ulgen <77659031+ulgensipahioglu@users.noreply.github.com>
2025-12-03 22:45:27 +0300

    Add files via upload

---
+++ sql.10.sql
@@ -0,0 +1,17 @@
+-- 1-
+/*
+SELECT city.city, country.country FROM city
+LEFT JOIN country ON city.country_id = country.country_id;
+*/
+
+-- 2-
+/*
+SELECT payment_id, first_name, last_name FROM payment
+RIGHT JOIN customer ON customer.customer_id = payment.customer_id;
+*/
+
+-- 3-
+/*
+SELECT rental_id, first_name, last_name FROM customer
+FULL JOIN rental ON rental.customer_id = customer.customer_id;
+*/
